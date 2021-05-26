466c5ae5e3aaa66a90ff6d302bf60c81ffbb2757
Heshow91 <mr.hraihan@gmail.com>
2021-05-26 13:29:43 -0400

    arranged classes

--- src/com/intertech/interfacepractice/Employee.java
+++ src/com/intertech/interfacepractice/Employee.java
@@ -2,7 +2,7 @@ package com.intertech.interfacepractice;
 
 import com.intertech.polymorphism.Person;
 
-public abstract class Employee extends Person {
+public abstract class Employee extends Person implements Payable{
 	public double pay() {
 		return 0;
 	}
@@ -14,7 +14,7 @@ public abstract class Employee extends Person {
 	}
 
 	public static void main(String[] args) {
-		Person p = new Employee();
-		System.out.println(p.getDescription());
-	}
+		//Person p = new Employee();
+		//System.out.println(p.getDescription());
+	//}
 }

fb073a8b70559fdafc3013de76696944f575fab4
Heshow91 <mr.hraihan@gmail.com>
2021-07-03 19:12:56 -0400

    added a new line

---
+++ src/com/udemy/javaTest/ArraysExample.java
@@ -0,0 +1,17 @@
+package com.udemy.javaTest;
+
+public class ArraysExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 5;
+		// will always do the do portion first then check condition
+		do{
+			System.out.println(num-- + " ");
+		}while(num ==0);
+
+		//check condition
+				
+	}
+
+}
